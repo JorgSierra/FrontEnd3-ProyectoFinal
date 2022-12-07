9d737ceaab1208bb5cc53ee11a30211f7ce8cb97
JorgSierra <jorge.sierrar@gmail.com>
2022-12-06 17:16:33 -0800

    Theme state added to Contact and Favs

--- src/Routes/Contact.jsx
+++ src/Routes/Contact.jsx
@@ -1,13 +1,18 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import Form from '../Components/Form'
+import { ContextGlobal } from '../Components/utils/global.context'
 
 //Este componente debera ser estilado como "dark" o "light" dependiendo del theme del Context
 
 const Contact = () => {
+  const { state } = useContext(ContextGlobal);
+
   return (
     <div>
       <h2>Want to know more?</h2>
       <p>Send us your questions and we will contact you</p>
+      <p>Current state: {state.currentState}</p>
+      <p>Next state: {state.nextState}</p>
       <Form/>
     </div>
   )
--- src/Routes/Favs.jsx
+++ src/Routes/Favs.jsx
@@ -1,13 +1,16 @@
-import React from "react";
-import Card from "../Components/Card";
+import React, { useContext } from "react";
+import { ContextGlobal } from "../Components/utils/global.context";
 
 //Este componente debera ser estilado como "dark" o "light" dependiendo del theme del Context
 
 const Favs = () => {
+  const { state } = useContext(ContextGlobal);
 
   return (
     <>
       <h1>Dentists Favs</h1>
+      <p>Current state: {state.currentState}</p>
+      <p>Next state: {state.nextState}</p>
       <div className="card-grid">
         {/* este componente debe consumir los destacados del localStorage */}
         {/* Deberan renderizar una Card por cada uno de ellos */}
