81da1845bec85fcb4c52df0131544c3786211bfc
pitis6 <felipecalvache6@gmail.com>
2022-12-07 19:11:23 -0500

    subiendo validaciones de form incompleta

--- src/Components/Form.jsx
+++ src/Components/Form.jsx
@@ -2,33 +2,38 @@ import { Button, FormControl, FormGroup, FormHelperText, Input, InputLabel, Typo
 import axios from 'axios'
 import React, { Fragment, useState } from 'react'
 import SendIcon from '@mui/icons-material/Send';
+import { useEffect } from 'react';
 
 
 const Form = () => {
   //Aqui deberan implementar el form completo con sus validaciones
 
-  const [inputValues, setInputValues] = useState({})
-  const [isValid, setIsValid] = useState(true)
-
-  const nameValidation =(e)=>{
-    if(e.target.value.length >= 1 && e.target.value.length <= 5){
-      setIsValid(false)
+  const [inputValues, setInputValues] = useState({});
+  const [nameIsValid, setNameIsValid] = useState(true);
+  const [emailIsValid, setEmailIsValid] = useState(true);
+  const [enableButton, setEnableButton] = useState(false);
+  
+  const nameValidation = (e) => {
+    if (e.target.value.length >= 1 && e.target.value.length <= 5) {
+      setNameIsValid(false)
       console.log(e.target.value.length);
-      console.log(isValid);
+      console.log(nameIsValid);
     }
-    else{
-      setIsValid(true)
+    else {
+      setNameIsValid(true);
     }
   }
 
   const emailValidation = (e) => {
-    if(e.target.value.length === RegExp(`^[^@]+@[^@]+[a-zA-Z]{2,}$`)){
-      setIsValid(false)
-      console.log(e.target.value.length);
-      console.log(isValid);
+    const reg = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/
+    const text = e.target.value;
+    const result = reg.test(text);
+
+    if(!result && e.target.value.length ){
+      setEmailIsValid(false)
     }
-    else{
-      setIsValid(true)
+    else {
+      setEmailIsValid(true)
     }
   }
 
@@ -38,33 +43,42 @@ const Form = () => {
       .catch(err => console.log(err.data))
   }
 
+  useEffect(()=>{
+    if(nameIsValid && emailIsValid){
+      setEnableButton(false);
+    }
+    else{
+      setEnableButton(true);
+    }
+  },[nameIsValid, emailIsValid])
+
 
   return (
     <Fragment>
-    <Typography variant='h4' sx={{ marginTop: '20px' }}>{'Contact'}</Typography>
-    <FormGroup sx={{ width: '300px', marginTop: '20px', gap: '10px' }}>
-      <FormControl>
-        <InputLabel error ={!isValid}>{isValid ? 'Full name' : 'Error'}</InputLabel>
-        <Input
-          error = {!isValid}
-          name='Full name'
-          onChange={(e) => nameValidation(e)}
-        />
-        <FormHelperText error ={!isValid}>{isValid ? '' : 'El nombre debe contener mas de 5 letras'}</FormHelperText>
-      </FormControl>
-      <FormControl>
-        <InputLabel error ={!isValid}>{isValid ? 'Email' : 'Error'}</InputLabel>
-        <Input
-          error = {!isValid}
-          name='Email'
-          type='email'
-          onChange={(e) => emailValidation(e)}
-        />
-        <FormHelperText error ={!isValid}>{isValid ? '' : 'Debe ingresar un correo valido'}</FormHelperText>
-      </FormControl>
-      <Button onClick={registrar} variant="contained" endIcon={<SendIcon />}>Send</Button>
-    </FormGroup>
-  </Fragment>
+      <Typography variant='h4' sx={{ marginTop: '20px' }}>{'Contact'}</Typography>
+      <FormGroup sx={{ width: '300px', marginTop: '20px', gap: '10px' }}>
+        <FormControl>
+          <InputLabel error={!nameIsValid}>{nameIsValid ? 'Full name' : 'Error'}</InputLabel>
+          <Input
+            error={!nameIsValid}
+            name='Full name'
+            onChange={(e) => nameValidation(e)}
+          />
+          <FormHelperText error={!nameIsValid}>{nameIsValid ? '' : 'El nombre debe contener mas de 5 letras'}</FormHelperText>
+        </FormControl>
+        <FormControl>
+          <InputLabel error={!emailIsValid}>{emailIsValid ? 'Email' : 'Error'}</InputLabel>
+          <Input
+            error={!emailIsValid}
+            name='Email'
+            coomponent='email'
+            onChange={(e) => emailValidation(e)}
+          />
+          <FormHelperText error={!emailIsValid}>{emailIsValid ? '' : 'Debe ingresar un correo valido'}</FormHelperText>
+        </FormControl>
+        <Button disabled={enableButton} onClick={registrar} variant="contained" endIcon={<SendIcon />}>Send</Button>
+      </FormGroup>
+    </Fragment>
   );
 };
 
